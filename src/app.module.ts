dfbc23e6cb56ee2ae8745c6d4d8fbfc46bfdb7cb
Johan <prisojohan2@gmail.com>
2024-10-26 13:29:06 +0100

    feat(logger): integrate Winston and Morgan for application logging
    - Installed Winston and Morgan for structured logging
    - Configured Winston in winst-config.ts for log management
    - Imported and integrated logger configuration in appModule.ts
    - Linked logging setup in main.ts for centralized logging across the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { WinstonModule } from 'nest-winston';
+import { winstonLoggerConfig } from '../winston.logger';
 
 @Module({
-  imports: [],
+  imports: [    WinstonModule.forRoot(winstonLoggerConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,39 @@
 import { morganOption } from '../winston.logger';
 import { NestFactory } from '@nestjs/core';
-import { config } from '../convict-config';
+import { config } from 'convict-config';
 import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
 import morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: false,
+      }
+    }),
+  );
+  
+  app.setGlobalPrefix(config.get('basePath'));
+  
+  app.enableCors();
 
   const format = `\n\b:remote-addr - ':method :url HTTP/:http-version' :status :response-time ms - :res[content-length] ":referrer" ":user-agent"\n + '\n\b'`;
   app.use(morgan(format, morganOption));
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(config.get('port'));
 }
